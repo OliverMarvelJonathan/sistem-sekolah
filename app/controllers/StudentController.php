4b62413770051df6d3fedeab53144817e12785b1
Oliver Marvel Jonathan <olivermarveljonathan@gmail.com>
2026-01-29 13:02:41 +0700

    feat: implement simple router

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers;
+
+class StudentController
+{
+    public function index()
+    {
+        echo '<h1>Daftar Siswa</h1>';
+        echo '<h1>Menampilkan daftar siswa</h1>';
+    }
+    
+    public function create()
+    {
+        echo '<h1>Daftar Siswa</h1>';
+        echo '<h1>Menampilkan daftar siswa</h1>';
+    }
+}
+    
+?>
--- app/core/Router.php
+++ app/core/Router.php
@@ -2,12 +2,30 @@
 
 namespace App\Core;
 
+use App\Controllers\StudentController;
 class router
 {
-    
-    public function run(): void
+
+    public function run()
     {
-        // Routing logic goes here
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if($method == 'GET' && $uri == '/students'){
+           require_once 'App/controllers/StudentController.php';
+           $controler = new StudentController();
+            $controler->index();
+            return;
+        }
+        
+        if($method == 'GET' && $uri == '/students'){
+            require_once '../app/controllers/StudentController.php';
+           $controler = new StudentController();
+            $controler->index();
+            return;
+        }
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
     }
 
-}
+} 
