3a8d114c455ea3b9d4e5cdc3ccc656cfc5920fd8
Oliver Marvel Jonathan <olivermarveljonathan@gmail.com>
2026-01-29 13:56:28 +0700

    wip: implement router app

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,25 +5,39 @@ namespace App\Core;
 use App\Controllers\StudentController;
 class router
 {
-
+    private $routes = [];
+    public function add(string $method, string $uri, string $controller, string $function)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+            'function' => $function,
+        ];
+    }
     public function run()
     {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        if($method == 'GET' && $uri == '/students'){
-           require_once 'App/controllers/StudentController.php';
-           $controler = new StudentController();
-            $controler->index();
-            return;
+        foreach($this->routes as $router){
+            $pattern = str_replace(
+               '{id}',
+                '([0-9]+)',
+                $router['uri']
+            );
+            $pattern = '#^' . $pattern . '$#';
+            if(preg_match($pattern, $uri, $matches)){
+                require_once 'App/controllers/' . $route['controller'] . '.php';
+                array_shift($matches);
+                $controllerClass = 'App\Controllers\\' . $router['controller'];
+                $controller = new $controllerClass();
+                $function = $router['function'];
+                $controller->$function();
+                return;
+            }
         }
-        
-        if($method == 'GET' && $uri == '/students'){
-            require_once '../app/controllers/StudentController.php';
-           $controler = new StudentController();
-            $controler->index();
-            return;
-        }
+            
         http_response_code(404);
         echo '<h1>404 - Page Not Found</h1>';
     }
